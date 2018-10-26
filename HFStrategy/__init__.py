b9113c42585ef79f91c9f5d27056b0aa2a7aebd3
Cris Mihalache <me@f3rno.com>
2018-10-26 17:16:58 +0300

    (feature) ROC, RSI indicators

--- HFStrategy/__init__.py
+++ HFStrategy/__init__.py
@@ -3,4 +3,6 @@ name = 'HFStrategy'
 from HFStrategy.strategy import Strategy
 from HFStrategy.indicators.indicator import Indicator
 from HFStrategy.indicators.ema import EMA
+from HFStrategy.indicators.rsi import RSI
+from HFStrategy.indicators.roc import ROC
 from HFStrategy.backtest import execOffline
