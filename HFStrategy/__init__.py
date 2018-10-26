d76d725d6255a41c96492d7d696bf8b241cb823b
Cris Mihalache <me@f3rno.com>
2018-10-26 20:50:20 +0300

    (feature) add Acceleration, AccumulationDistribution

--- HFStrategy/__init__.py
+++ HFStrategy/__init__.py
@@ -5,4 +5,6 @@ from HFStrategy.indicators.indicator import Indicator
 from HFStrategy.indicators.ema import EMA
 from HFStrategy.indicators.rsi import RSI
 from HFStrategy.indicators.roc import ROC
+from HFStrategy.indicators.accumulation_distribution import AccumulationDistribution
+from HFStrategy.indicators.acceleration import Acceleration
 from HFStrategy.backtest import execOffline
