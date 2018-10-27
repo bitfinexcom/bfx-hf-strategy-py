bc80d941f22deade7bdd57d9b5eb122b4a3e0910
Cris Mihalache <me@f3rno.com>
2018-10-27 13:59:31 +0300

    (feature) add ATR, SMA, AO, BOP, StdDev, BBands

--- HFStrategy/__init__.py
+++ HFStrategy/__init__.py
@@ -8,5 +8,11 @@ from HFStrategy.indicators.roc import ROC
 from HFStrategy.indicators.accumulation_distribution import AccumulationDistribution
 from HFStrategy.indicators.acceleration import Acceleration
 from HFStrategy.indicators.alma import ALMA
+from HFStrategy.indicators.atr import ATR
 from HFStrategy.indicators.accumulative_swing_index import AccumulativeSwingIndex
+from HFStrategy.indicators.sma import SMA
+from HFStrategy.indicators.awesome_oscillator import AO
+from HFStrategy.indicators.balance_of_power import BOP
+from HFStrategy.indicators.stddev import StdDeviation
+from HFStrategy.indicators.bbands import BollingerBands
 from HFStrategy.backtest import execOffline
---
+++ HFStrategy/indicators/bbands.py
@@ -0,0 +1,50 @@
+from HFStrategy.indicators.indicator import Indicator
+from HFStrategy.indicators.sma import SMA
+from HFStrategy.indicators.stddev import StdDeviation
+
+class BollingerBands(Indicator):
+  def __init__(self, args = []):
+    [ period, mul ] = args
+
+    self._p = period
+    self._m = mul
+    self._sma = SMA([period])
+    self._stddev = StdDeviation([period])
+
+    super().__init__({
+      'args': args,
+      'id': 'bbands',
+      'name': 'BBANDS(%f, %f)' % (period, mul),
+      'seedPeriod': period
+    })
+
+  def reset(self):
+    super().reset()
+    self._sma.reset()
+    self._stddev.reset()
+
+  def update(self, v):
+    self._sma.update(v)
+    self._stddev.update(v)
+
+    middle = self._sma.v()
+    stddev = self._stddev.v()
+
+    return super().update({
+      'top': middle + (self._m * stddev),
+      'middle': middle,
+      'bottom': middle - (self._m * stddev)
+    })
+
+  def add(self, v):
+    self._sma.add(v)
+    self._stddev.add(v)
+
+    middle = self._sma.v()
+    stddev = self._stddev.v()
+
+    return super().add({
+      'top': middle + (self._m * stddev),
+      'middle': middle,
+      'bottom': middle - (self._m * stddev)
+    })
