6b84c45e0c0d005b00e011ddcd42340d2842ea67
Jacob Plaster <jacob.plaster@youview.com>
2018-11-06 11:25:21 +0000

    Manage trades with position object

--- HFStrategy/Strategy/Position.py
+++ HFStrategy/Strategy/Position.py
@@ -1,17 +1,53 @@
+import datetime
 
 class Position:
-  def __init__(self, symbol, amount, price, trades=[], stop=None, target=None, tag=''):
+  def __init__(self, symbol, stop=None, target=None, tag=''):
     self.symbol = symbol
-    self.price = price
-    self.amount = amount
-    self.trades = trades
     self.stop = stop
     self.target = target
     self.tag = tag
+    self.date = datetime.datetime.now()
+
+    self.price = 0
+    self.profitLoss = 0
+    self.amount = 0
+    self.totalFees = 0
+    self.trades = []
+
+    self._isOpen = True
+
+  def addTrade(self, trade):
+    tradeNV = trade.amount * trade.price
+    totalAmount = self.amount + trade.amount
+    posNV = self.amount * self.price
+
+    self.trades += [trade]
+    self.totalFees += trade.fee
+    self.profitLoss = posNV + tradeNV
+
+    if len(self.trades) == 0:
+      self.amount = trade.amount
+      self.price = trade.price
+      return
+
+    if totalAmount == 0:
+      self.price = 0
+      self.amount = 0
+      return
+
+    self.price = (posNV + tradeNV) / totalAmount
+    self.amount += trade.amount
+
+  def close(self):
+    self._isOpen = False
+
+  def isOpen(self):
+    return self._isOpen
   
   def __str__(self):
     ''' Allow us to print the Position object in a pretty format '''
-    mainStr = "Position <'{0}' x {1} @ {2}".format(self.symbol, self.amount, self.price)
+    mainStr = "Position <'{}' x {} @ {} P&L={}".format(
+      self.symbol, self.amount, self.price, self.profitLoss)
     if self.stop != None:
         mainStr += " stop={}".format(self.stop)
     if self.target:
@@ -19,6 +55,6 @@ class Position:
     if self.tag:
       mainStr += " tag={}".format(self.tag)
     # format trades into string
-    mainStr += " tradeCount={}>".format(len(self.trades))
+    mainStr += " tradeCount={} isOpen={}>".format(len(self.trades), self._isOpen)
     return mainStr
 
