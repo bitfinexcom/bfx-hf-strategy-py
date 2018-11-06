d4e69eb4a89cd25cac2f25ca68da4250e0e318aa
Jacob Plaster <jacob.plaster@youview.com>
2018-11-06 14:37:52 +0000

    backtest: calculate more performance results

--- HFStrategy/utils/backtest.py
+++ HFStrategy/utils/backtest.py
@@ -1,4 +1,7 @@
 from prettytable import PrettyTable
+from .CustomLogger import CustomLogger
+
+logger = CustomLogger('HFBacktest', logLevel='INFO')
 
 def logTrades(positions):
   x = PrettyTable()
@@ -7,13 +10,13 @@ def logTrades(positions):
   for pos in positions:
     for i, t in enumerate(pos.trades):
       lastItem = i+1 == len(pos.trades)
-      pl = round(pos.profitLoss, 2)
-      x.add_row([t.date, pos.symbol, t.direction, t.amount, round(t.price, 2),
+      pl = round(pos.netProfitLoss, 2)
+      x.add_row([t.date, pos.symbol, t.direction, abs(t.amount), round(t.price, 2),
                 round(t.fee, 2), pl if lastItem else 0, ''])
 
   print(x)
 
-def execOffline(candles, trades, strategy):
+def execOffline(candles, strategy):
   for candle in candles:
     strategy.onCandle(candle)
   strategy.closeOpenPositions()
@@ -23,17 +26,42 @@ def execOffline(candles, trades, strategy):
   profitLoss = 0
   totalFees = 0
   totalTrades = 0
+  totalVolume = 0
   positions = strategy.closedPositions
+  minProfitLoss = 0
+  maxProfitLoss = 0
+  totalLossesCount = 0
+  totalLosses = 0
+  totalGainersCount = 0
+  totalGainers = 0
+
   for pos in positions:
     profitLoss += pos.profitLoss
     totalFees += pos.totalFees
     totalTrades += len(pos.trades)
+    totalVolume += pos.volume
+    if pos.netProfitLoss < 0:
+      totalLossesCount += 1
+      totalLosses += pos.netProfitLoss
+    else:
+      totalGainersCount += 1
+      totalGainers += pos.netProfitLoss
+    netP = pos.netProfitLoss
+    minProfitLoss = netP if netP < minProfitLoss else minProfitLoss
+    maxProfitLoss = netP if netP > maxProfitLoss else maxProfitLoss
   
   logTrades(positions)
-  
-  print ("\nGross Profit/loss: {}".format(profitLoss))
-  print ("Fees: {}".format(totalFees))
-  print ("Net Profit/loss: {}".format(profitLoss - totalFees))
+  print('')
 
-  print ("{} Positions | {} Trades".format(len(positions), totalTrades))
+  totalNetProfitLoss = profitLoss - totalFees
+  logger.info("Net P/L {} | Gross P/L {} | Vol {} | Fees {}".format(
+              round(totalNetProfitLoss, 2), round(profitLoss, 2),
+              round(totalVolume, 2), round(totalFees, 2)))
+  logger.info("Min P/L {} | Max P/L {} | Avg P/L {}".format(
+              round(minProfitLoss, 2), round(maxProfitLoss, 2), 
+              round(totalNetProfitLoss / totalTrades, 2)))
+  logger.info("Losses {} (total {}) | Gains {} (total {})".format(
+              totalLossesCount, round(totalLosses, 2), totalGainersCount,
+              round(totalGainers, 2)))
+  logger.info("{} Positions | {} Trades".format(len(positions), totalTrades))
 
