78c356f9d1d0746d3fabd91a5b5482a364b07f2c
Jacob Plaster <jacob.plaster@youview.com>
2018-11-06 11:25:38 +0000

    Store more variables in Order/Trade objects

--- HFStrategy/Strategy/Order.py
+++ HFStrategy/Strategy/Order.py
@@ -4,10 +4,11 @@ def now_in_mills():
   return int(round(time.time() * 1000))
 
 class Order:
-  def __init__(self, symbol, price, mtsCreate=now_in_mills()):
+  def __init__(self, symbol, amount, price, mtsCreate=now_in_mills()):
     self.symbol = symbol
     self.priceAvg = price
     self.mtsCreate = mtsCreate
+    self.amount = amount
   
   def __str__(self):
     ''' Allow us to print the Order object in a pretty format '''
