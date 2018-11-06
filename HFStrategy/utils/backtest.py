65bca448ac76a90547d88f9e5bbe549b2ee2382d
Jacob Plaster <jacob.plaster@youview.com>
2018-11-06 11:28:14 +0000

    backtest: Adds printout of table and extra data

--- HFStrategy/utils/backtest.py
+++ HFStrategy/utils/backtest.py
@@ -1,25 +1,40 @@
+from prettytable import PrettyTable
+
+def logTrades(positions):
+  x = PrettyTable()
+  x.field_names = ["Date", "Symbol", "Direction", "Amount", "Price", "Fee", "P&L", "Label"]
+
+  for pos in positions:
+    for i, t in enumerate(pos.trades):
+      lastItem = i+1 == len(pos.trades)
+      pl = round(pos.profitLoss, 2)
+      x.add_row([t.date, pos.symbol, t.direction, t.amount, round(t.price, 2),
+                round(t.fee, 2), pl if lastItem else 0, ''])
+
+  print(x)
+
 def execOffline(candles, trades, strategy):
   currentTrade = 0
 
   for candle in candles:
-    while currentTrade < len(trades) and trades[currentTrade]['mts'] < candle['mts']:
-      strategy.onTrade(trades[currentTrade])
-      currentTrade += 1
-
     strategy.onCandle(candle)
 
   print ("\nBacktesting complete: \n")
 
-  # TODO: calculate fees and averages ect...
-  # TODO: calculate profit and loss correctly
-
   profitLoss = 0
-  for pos in strategy.closedPositions:
-    print (pos)
-
-    if pos.amount < 0:
-      profitLoss -= pos.price
-    else:
-      profitLoss += pos.price
+  totalFees = 0
+  totalTrades = 0
+  positions = strategy.closedPositions
+  for pos in positions:
+    profitLoss += pos.profitLoss
+    totalFees += pos.totalFees
+    totalTrades += len(pos.trades)
   
-  print ("\nProfit/loss: {}".format(profitLoss))
+  logTrades(positions)
+  
+  print ("\nGross Profit/loss: {}".format(profitLoss))
+  print ("Fees: {}".format(totalFees))
+  print ("Net Profit/loss: {}".format(profitLoss - totalFees))
+
+  print ("{} Positions | {} Trades".format(len(positions), totalTrades))
+
