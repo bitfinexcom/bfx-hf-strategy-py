b019ac2070fccf17c850fc5f6e0327bc3df5fae0
Jacob Plaster <jacob.plaster@youview.com>
2018-12-21 14:49:22 +0000

    MockWebsocket: run emit functon synchronously

--- hfstrategy/utils/MockWebsocketClient.py
+++ hfstrategy/utils/MockWebsocketClient.py
@@ -7,11 +7,15 @@ class MockWebsocket:
   def on(self, *args, **kwargs):
     self.events.on(*args, **kwargs)
 
-  def _emit(self, *args, **kwargs):
-    self.events.emit(*args, **kwargs)
+  async def _emit(self, event, *args, **kwargs):
+    listeners = self.events.listeners(event)
+    await asyncio.gather(*[f(*args, **kwargs) for f in listeners])
 
   def remove_all_listeners(self, *args, **kwargs):
     self.events.remove_all_listeners(*args, **kwargs)
+
+  async def cancel_order(self, *args, **kawargs):
+    pass
 
 class MockClient:
   ws = MockWebsocket()
