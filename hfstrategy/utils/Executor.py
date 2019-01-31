068d612d4ced61158cf60fb3bf1cb7f0859bb603
Jacob Plaster <jacob.plaster@youview.com>
2019-01-31 12:22:02 +0000

    Executor: fix backtest_data_server

--- hfstrategy/utils/Executor.py
+++ hfstrategy/utils/Executor.py
@@ -51,7 +51,8 @@ def _logTrades(positions):
 
 def _finish(strategy):
   print ("\nBacktesting complete: \n")
-  profit_loss = 0
+  total_gross_profit_loss = 0
+  total_net_profit_loss = 0
   total_fees = 0
   totalTrades = 0
   totalVolume = 0
@@ -62,22 +63,24 @@ def _finish(strategy):
   totalLosses = 0
   totalGainersCount = 0
   totalGainers = 0
-  net_pl = 0
 
   for pos in positions:
-    profit_loss += pos.get_profit_loss()['realised']
+    current_gross_pl = pos.get_profit_loss()['gross']
+    total_gross_profit_loss += current_gross_pl
+    total_net_profit_loss += pos.get_profit_loss()['net']
     total_fees += pos.total_fees
     totalTrades += len(pos.orders)
     totalVolume += pos.volume
-    net_pl = pos.get_profit_loss()['net']
-    if net_pl < 0:
+    net_pl = pos.get_realised_profit_loss()
+
+    if current_gross_pl < 0:
       totalLossesCount += 1
-      totalLosses += net_pl
+      totalLosses += current_gross_pl
     else:
       totalGainersCount += 1
-      totalGainers += net_pl
-    minProfitLoss = net_pl if net_pl < minProfitLoss else minProfitLoss
-    maxProfitLoss = net_pl if net_pl > maxProfitLoss else maxProfitLoss
+      totalGainers += current_gross_pl
+    minProfitLoss = current_gross_pl if current_gross_pl < minProfitLoss else minProfitLoss
+    maxProfitLoss = current_gross_pl if current_gross_pl > maxProfitLoss else maxProfitLoss
   
   if not positions:
     logger.info("No closed positions recorded.")
@@ -87,11 +90,11 @@ def _finish(strategy):
   print('')
 
   logger.info("Net P/L {} | Gross P/L {} | Vol {} | Fees {}".format(
-    round(net_pl, 2), round(profit_loss, 2),
+    round(total_net_profit_loss, 2), round(total_gross_profit_loss, 2),
     round(totalVolume, 2), round(total_fees, 2)))
   logger.info("Min P/L {} | Max P/L {} | Avg P/L {}".format(
     round(minProfitLoss, 2), round(maxProfitLoss, 2), 
-    round(net_pl / totalTrades, 2)))
+    round(total_gross_profit_loss / totalTrades, 2)))
   logger.info("Losses {} (total {}) | Gains {} (total {})".format(
     totalLossesCount, round(totalLosses, 2), totalGainersCount,
     round(totalGainers, 2)))
@@ -108,16 +111,18 @@ async def _seed_candles(strategy, bfxapi, tf):
 
 class Executor:
 
-  def __init__(self, strategy, timeframe='1hr'):
+  def __init__(self, strategy, timeframe='1hr', show_chart=True):
     self.strategy = strategy
-    self.stored_prices = []
+    self.stored_prices = {}
     self.timeframe = timeframe
+    self.show_chart = show_chart
 
   def _store_candle_price(self, candle):
-    self.stored_prices += [(candle['close'], candle['mts'])]
+    self.stored_prices[candle['mts']] = candle['close']
 
   def _draw_chart(self):
-    show_orders_chart(self.stored_prices, self.strategy)
+    if (self.show_chart):
+      show_orders_chart(self.stored_prices, self.strategy)
 
   def _kill_signal_handler(self, sig, frame):
     _finish(self.strategy)
@@ -133,7 +138,8 @@ class Executor:
       API_SECRET,
       manageOrderBooks=True, # verify orderbook locally
       dead_man_switch=True, # Kill all orders on disconnect
-      logLevel=self.strategy.logLevel
+      logLevel=self.strategy.logLevel,
+      max_retries=120 # connection retry 120 times before exiting (10 mins)
     )
     bfxOrderManager = OrderManager(bfx, backtesting=backtesting, logLevel=self.strategy.logLevel)
     self.strategy.set_order_manager(bfxOrderManager)
@@ -146,6 +152,7 @@ class Executor:
       await bfx.ws.subscribe('book', self.strategy.symbol)
     # bind events
     bfx.ws.on('connected', subscribe)
+    bfx.ws.on('connected', self.strategy._connected)
     bfx.ws.on('new_candle', self.strategy._process_new_candle)
     bfx.ws.on('new_candle', self._store_candle_price)
     bfx.ws.on('new_trade', self.strategy._process_new_trade)
@@ -156,15 +163,18 @@ class Executor:
     def end():
       _finish(self.strategy)
       self._draw_chart()
-    ws = DataServerWebsocket(symbol=self.strategy.symbol)
+    ws = DataServerWebsocket(logLevel=self.strategy.logLevel)
     self.strategy.ws = ws
     ws.on('done', end)
     ws.on('new_candle', self.strategy._process_new_candle)
     ws.on('new_candle', self._store_candle_price)
     ws.on('new_trade', self.strategy._process_new_trade)
-    self.strategy.orderManager = OrderManager(ws, backtesting=True, logLevel='INFO')
+    # mock order simulation
+    bfx = MockClient()
+    bfxOrderManager = OrderManager(bfx, backtesting=True, logLevel='INFO')
+    self.strategy.set_order_manager(bfxOrderManager)
     self.strategy.backtesting = True
-    self.strategy.ws.run(fromDate, toDate, trades, candles, self.timeframe, candleFields, tradeFields, sync)
+    self.strategy.ws.run(self.strategy.symbol, fromDate, toDate, trades, candles, self.timeframe, candleFields, tradeFields, sync)
 
   def offline(self, file=None, candles=None):
     bfx = MockClient()
@@ -182,7 +192,8 @@ class Executor:
           candleArray[4], candleArray[5], self.strategy.symbol, self.timeframe
         ) for candleArray in candleData]
         # save candles so we can draw a chart later on
-        self.stored_prices= [(c['close'], c['mts']) for c in candles]
+        for c in candles:
+          self.stored_prices[c['mts']] = c['close']
         # run async event loop
         loop = asyncio.get_event_loop()
         task = asyncio.ensure_future(_process_candle_batch(self.strategy, candles))
